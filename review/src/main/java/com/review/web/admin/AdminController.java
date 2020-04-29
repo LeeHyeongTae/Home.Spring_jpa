bda171fff3873f9dc56ade5b555740caee2f684f
LeeHyengTae <tedd911@icloud.com>
2020-04-29 11:43:39 +0900

    11:43

--- review/src/main/java/com/review/web/admin/AdminController.java
+++ review/src/main/java/com/review/web/admin/AdminController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.review.web.util.Messenger;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admins")
 public class AdminController {
 	@Autowired AdminService adminService;
 	
