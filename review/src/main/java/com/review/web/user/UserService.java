1137459dd26b48de7ca9ba37de2e9834b355d40f
LeeHyengTae <tedd911@icloud.com>
2020-04-29 13:17:34 +0900

    13:17 search branch reset

--- review/src/main/java/com/review/web/user/UserService.java
+++ review/src/main/java/com/review/web/user/UserService.java
@@ -1,15 +1,6 @@
 package com.review.web.user;
 
-import java.util.List;
 
 public interface UserService {
 
-	public void add(User user);
-	public int count();
-	public User login(User user);
-	public User detail(String userid);
-	public boolean update(User user);
-	public boolean remove(String userid);
-	public List<User> list();
-	public boolean idSearch(String user);
 }
--- review/src/main/java/com/review/web/user/UserServiceImpl.java
+++ review/src/main/java/com/review/web/user/UserServiceImpl.java
@@ -1,60 +1,9 @@
 package com.review.web.user;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
-	@Override
-	public void add(User user) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public User login(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User detail(String userid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean update(User user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean remove(String userid) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<User> list() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean idSearch(String user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
 }
