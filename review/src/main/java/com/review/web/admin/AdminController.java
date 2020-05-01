7cba8a2294f68a93f9b9deed06d7fe5fe6ea3652
LeeHyengTae <tedd911@icloud.com>
2020-05-01 14:50:22 +0900

    14:50 new branch

--- review/src/main/java/com/review/web/admin/AdminController.java
+++ review/src/main/java/com/review/web/admin/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.review.web.util.Messenger;
-
+//di commit
 @RestController
 @RequestMapping("/admins")
 public class AdminController {
